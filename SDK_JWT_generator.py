7563382c9d67f143f6bd434c5415d7bde9d4a3c9
Elhai Agassi <70665375+ElhaiAgassi@users.noreply.github.com>
2024-05-24 13:53:58 +0300

    make it get a name and print key

--- SDK_JWT_generator.py
+++ SDK_JWT_generator.py
@@ -1,28 +1,44 @@
-import jwt
 import datetime
-from config import SDK_KEY,SDK_SECRET
+import sys
+import jwt
+from config import SDK_KEY, SDK_SECRET
 
-def generate_sdk_jwt():
-    # Current UTC time
+def generate_sdk_jwt(session_name="mysession"):
+    print ("The session name is: "+session_name)
+
     now_utc = datetime.datetime.utcnow()
-    # Token expiration time
-    token_expiration = now_utc + datetime.timedelta(days=30)  # 1 hour from now
+    token_expiration = now_utc + datetime.timedelta(hours=40)  # 40 hours from now. (limited to 48)
+
+   # Convert datetime to integer timestamps
+    iat_timestamp = int(now_utc.timestamp())
+    exp_timestamp = int(token_expiration.timestamp())
 
     # JWT Header
     header = {
-        "alg": "HS256",
-        "typ": "JWT"
+	    "typ": "JWT",
+        "alg": "HS256"
     }
 
     # JWT Payload
     payload = {
-        "aud": None,  # Set audience to null
-        "iss": SDK_KEY,  # Use "iss" for the API key
-        "exp": int(token_expiration.timestamp()),
-        "iat": int(now_utc.timestamp())
+	    "app_key": SDK_KEY,
+        "role_type": 1,  # 1 is an host, 0 is participant
+        "tpc": session_name,  # Add session name to the payload as 'tpc'.
+	    "version": 1,
+        "iat": iat_timestamp, # The token issue timestamp.
+        "exp": exp_timestamp , # The JWT expiration timestamp.
+        
     }
 
     # Generating the JWT
     token = jwt.encode(payload, SDK_SECRET, algorithm='HS256', headers=header)
 
     return token
+
+if __name__ == "__main__":
+    session_name = sys.argv[1] if len(sys.argv) > 1 else None
+    if session_name is None:
+        print("No session name provided. Default - \"mysession\"")
+    token = generate_sdk_jwt(session_name)
+    print("Key:\n"+token)
+
